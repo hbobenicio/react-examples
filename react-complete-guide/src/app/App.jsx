fb0c61db284421cc2a5674ed49e9c503e1ca4688
Hugo Benicio <hbobenicio@gmail.com>
2018-05-03 12:25:34 -0300

    Adding remove person to react-complete-guide

--- react-complete-guide/src/app/App.jsx
+++ react-complete-guide/src/app/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css'
 
 import Person from './Person/Person'
 
@@ -24,8 +25,8 @@ class App extends Component {
     })
   }
 
-  nameChangedHandler = (id, event) => {
-    const personIndex = this.state.people.findIndex(p => p.id === id)
+  nameChangedHandler = (personId, event) => {
+    const personIndex = this.state.people.findIndex(p => p.id === personId)
     const newPerson = {
       ...this.state.people[personIndex],
       name: event.target.value
@@ -45,22 +46,31 @@ class App extends Component {
     })
   }
 
+  removedPersonHandler = (id, event) => {
+    const people = [...this.state.people]
+    people.splice(id, 1)
+
+    this.setState({
+      people
+    })
+  }
+
   renderPeople = () => {
     let people = null
 
     if (this.state.showPeople) {
-      const personTemplate = person =>
-        <Person key={person.id}
-          name={person.name} age={person.age}
-          nameChangeHandler={(event) => this.nameChangedHandler(person.id, event)} />
-      // const personTemplate = person =>
-      //   <Person key={person.id}
-      //     name={person.name} age={person.age}
-      //     nameChangeHandler={this.nameChangedHandler.bind(this, person.id)} />
-        
-      people = <div>{
-        this.state.people.map(personTemplate)
-      }</div>
+      people =
+        <div>
+          {
+            this.state.people.map((person, index) =>
+              <Person key={person.id}
+                name={person.name} age={person.age}
+                nameChangeHandler={(event) => this.nameChangedHandler(person.id, event)}
+                removeHandler={(event) => this.removedPersonHandler(index, event)}/>
+                //  nameChangeHandler={this.nameChangedHandler.bind(this, person.id)} />
+            )
+          }
+        </div>
     }
 
     return people
@@ -68,8 +78,8 @@ class App extends Component {
 
   render = () => {
     // inline style, one way to scope stylings.
-    // You loose css powers though. There is
-    // a better way to scope css...
+    // You loose css powers though (like pseudo selectors).
+    // There is a better way to scope css...
     const buttonStyle = {
       backgroundColor: 'green',
       color: 'white',
@@ -81,14 +91,30 @@ class App extends Component {
       textAlign: 'center'
     }
 
+    if (this.state.showPeople) {
+      buttonStyle.backgroundColor = 'red'
+    }
+
+    const classes = []
+    if (this.state.people.length <= 2) {
+      classes.push('red')
+    }
+    if (this.state.people.length <= 1) {
+      classes.push('bold')
+    }
+
     return (
       <div id="App">
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
 
         <div style={{textAlign: 'center'}}>
-          <button type="button" style={buttonStyle}
+          <button type="button"
+            key='btn1' style={buttonStyle}
             onClick={this.swapNameHandler}>Swap Names</button>
 
-          <button type="button" style={buttonStyle}
+          <button type="button"
+            key='btn2' style={buttonStyle}
             onClick={this.peopleToggledHandler}>Toggle People</button>
         </div>
 
@@ -98,4 +124,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App
--- react-complete-guide/src/app/Person/Person.jsx
+++ react-complete-guide/src/app/Person/Person.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import './Person.css'
 
 const person = (props) => {
+
   return (
     <div className="Person">
+      <div className="action-bar">
+        <button type="text" onClick={props.removeHandler}>X</button>
+      </div>
+
       <p>I'm a {props.name} and I'm {props.age} years old.</p>
       {props.children}
       <input type="text" onChange={props.nameChangeHandler} />
